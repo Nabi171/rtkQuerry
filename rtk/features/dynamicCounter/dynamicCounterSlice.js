21fbef0346432c32a7cc8d6092f59b73f2d968c1
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-04 21:41:56 +0600

    chang in counter

---
+++ rtk/features/dynamicCounter/dynamicCounterSlice.js
@@ -0,0 +1,21 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+// initial state
+const initialState = {
+    count: 0,
+};
+
+const dynamicCounterSlice = createSlice({
+    name: "dynamiCounter",
+    initialState,
+    reducers: {
+        increment: (state, action) => {
+            state.count += action.payload;
+        },
+        decrement: (state, action) => {
+            state.count -= action.payload;
+        },
+    },
+});
+module.exports = dynamicCounterSlice.reducer;
+module.exports.dynamicCounterActions = dynamicCounterSlice.actions;
