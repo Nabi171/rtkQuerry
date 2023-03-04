7f71858c2861dab25f21c8d17585173ff954facd
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-04 23:25:59 +0600

    chang in countr

--- rtk/index.js
+++ rtk/index.js
@@ -2,11 +2,11 @@ const store = require("./app/store");
 const { counterActions } = require("./features/counter/counterSlice");
 const { dynamicCounterActions } = require("./features/dynamicCounter/dynamicCounterSlice")
 // initial state
-console.log(`Initial State: ${JSON.stringify(store.getState())}`);
+// console.log(`Initial State: ${JSON.stringify(store.getState())}`);
 
 // subscribe to state changes
 store.subscribe(() => {
-    console.log(store.getState());
+    // console.log(store.getState());
 });
 
 // disptach actions
